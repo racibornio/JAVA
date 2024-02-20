60fc877aee8d49b62f0e2bf1a485511a3c0f9d67
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2024-02-20 21:58:43 +0100

    Description for shift-left and shift-right added.

--- src/main/java/com/patryk/kompendium/BitShift.java
+++ src/main/java/com/patryk/kompendium/BitShift.java
@@ -34,6 +34,10 @@ public class BitShift {
             System.out.println(num);
         }
 
+        // every shift-left moves bits by one position - for int each after 31st gets lost, for long each after 63rd
+        // moving value '1' onto 31st or 63rd accordingly and the value will get negative
+        // this will always bring new '0' value on the most left bit
+
 
         //////////////////////////////////////////////////////////////
         // right shift
@@ -42,6 +46,19 @@ public class BitShift {
         System.out.println(rightA);
         rightA = rightA >> 2;
         System.out.println(rightA);
+
+        // every shift-right moves bits by one position
+        // this will always bring new '1' value on the most right bit
+
+        //////////////////////////////////////////////////////////////
+        // when a negative value is unwanted
+
+        char[] hex = {
+                '0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+        };
+        byte bHex = (byte) 0xf1;
+        System.out.println("bHex = 0x" + hex[(bHex >> 4) & 0x0f] + hex[bHex & 0x0f]);
 
     }
 }
