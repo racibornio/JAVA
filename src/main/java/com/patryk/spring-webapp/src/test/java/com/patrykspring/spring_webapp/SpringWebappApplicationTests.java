e565e789e9aea96a5c5770e06b7a61c8a2e90219
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2024-08-30 16:02:17 +0200

    Spring init

---
+++ src/main/java/com/patryk/spring-webapp/src/test/java/com/patrykspring/spring_webapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrykspring.spring_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
